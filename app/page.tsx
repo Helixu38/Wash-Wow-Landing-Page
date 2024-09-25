4c0c7e3545923c5ae445281eb5baaf4aa4abeb2e
helixu38 <you@example.com>
2024-09-26 02:09:17 +0700

    feat: implement bottom content

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Content from "@/components/hero/content";
+import ContentBottom from "@/components/hero/contentbotom";
 import Hero from "@/components/hero/hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero></Hero>
       <Content></Content>
+      <ContentBottom></ContentBottom>
     </>
   );
 }
