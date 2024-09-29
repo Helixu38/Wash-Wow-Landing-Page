89aa9712c0e6a2e164f5757b2944c7230ea705a6
helixu38 <you@example.com>
2024-09-30 04:06:36 +0700

    build(fix): bypass build error

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
