33da6cf0481581ae39a082cb87ccc0a344559925
helixu38 <vipase2004@gmail.com>
2024-10-03 23:28:10 +0700

    feat: implement routing and term content

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -5,7 +5,7 @@ import { IconQuestionMark } from "@tabler/icons-react";
 
 export default function Navbar() {
   return (
-    <div className="w-full h-[88px] flex flex-row justify-between items-center px-4 md:px-[58px] font-medium text-xl text-primary">
+    <div className="w-full h-[88px] flex flex-row justify-between items-center px-4 md:px-[58px] font-medium text-xl text-primary shadow-md">
       <div className="flex items-center">
         <Link href="/">
           <AppLogo />
@@ -16,10 +16,14 @@ export default function Navbar() {
         <Link href={"/download"}>
           <p className="hidden md:block">Tải app ngay</p>
         </Link>
-        <p className="hidden md:block">Về chúng tôi</p>
-        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary">
-          <IconQuestionMark color="white" size={20} />
-        </div>
+        <Link href={"/about-us"}>
+          <p className="hidden md:block">Về chúng tôi</p>
+        </Link>
+        <Link href={"/question"}>
+          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary">
+            <IconQuestionMark color="white" size={20} />
+          </div>
+        </Link>
       </div>
     </div>
   );
--- components/term-and-condition/content.tsx
+++ components/term-and-condition/content.tsx
@@ -2,8 +2,127 @@ import React from "react";
 
 export default function Content() {
   return (
-    <div className="ml-[133px] mt-[129px] text-6xl text-primary font-bold mb-[417px]">
-      Điều khoản sử dụng dịch vụ
+    <div className="ml-[133px] mt-[129px] text-primary font-bold mb-[62px]">
+      <h1 className="text-6xl mb-8">Điều khoản sử dụng dịch vụ</h1>
+      <div className="text-xl leading-8">
+        <p className="mb-4 font-medium">
+          Chào mừng bạn đến với Wash&Wow! Khi bạn sử dụng dịch vụ của chúng tôi,
+          bạn đồng ý với các điều khoản sử dụng dưới đây. Chúng tôi khuyến khích
+          bạn đọc kỹ trước khi sử dụng để đảm bảo quyền lợi của bạn.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">1. Trách Nhiệm Của Wash&Wow</h3>
+
+        <h4 className="font-semibold mt-[20px]">
+          1.1 Cam Kết Cung Cấp Dịch Vụ
+        </h4>
+        <p className="mb-4 font-medium">
+          Wash&Wow cam kết cung cấp nền tảng đặt lịch giặt sấy và các dịch vụ
+          liên quan đúng hạn và đảm bảo chất lượng. Chúng tôi cố gắng hết sức để
+          đảm bảo sự chính xác trong thông tin dịch vụ và cửa hàng.
+        </p>
+
+        <h4 className="font-semibold">
+          1.2 Không Chịu Trách Nhiệm Về Hành Vi Của Các Đối Tác
+        </h4>
+        <p className="mb-4 font-medium">
+          Chúng tôi không can thiệp vào quá trình vận hành và chất lượng dịch vụ
+          của các cửa hàng giặt sấy đối tác. Wash&Wow không chịu trách nhiệm cho
+          các vấn đề phát sinh do cửa hàng đối tác cung cấp dịch vụ không đạt
+          yêu cầu.
+        </p>
+
+        <h4 className="font-semibold">1.3 Hỗ Trợ Khách Hàng</h4>
+        <p className="mb-[27px] font-medium">
+          Chúng tôi cung cấp kênh hỗ trợ thông qua ứng dụng và trang web để giải
+          đáp mọi thắc mắc và hỗ trợ kịp thời cho người dùng.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">
+          2. Trách Nhiệm Của Khách Hàng
+        </h3>
+
+        <h4 className="font-semibold ">2.1 Tuân Thủ Pháp Luật</h4>
+        <p className="mb-4 font-medium">
+          Khách hàng phải tuân thủ các quy định của pháp luật Việt Nam trong
+          suốt quá trình sử dụng dịch vụ của Wash&Wow.
+        </p>
+
+        <h4 className="font-semibold ">2.2 Cung Cấp Thông Tin Chính Xác</h4>
+        <p className="mb-[27px] font-medium">
+          Khách hàng có trách nhiệm cung cấp thông tin chính xác khi đăng ký tài
+          khoản và đặt dịch vụ, bao gồm họ tên, địa chỉ, và thông tin liên lạc.
+          Nếu có sự thay đổi, khách hàng cần cập nhật kịp thời để đảm bảo không
+          gián đoạn dịch vụ.
+        </p>
+
+        <h4 className="font-semibold mb-2">
+          2.3 Bảo Quản Dữ Liệu Và Bảo Mật Thông Tin
+        </h4>
+        <p className="mb-4 font-medium">
+          Khách hàng chịu trách nhiệm bảo mật thông tin đăng nhập và tài khoản
+          của mình. Nếu phát hiện có truy cập trái phép, vui lòng thông báo ngay
+          cho chúng tôi để được hỗ trợ kịp thời.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">
+          3. Các Hình Thức Sử Dụng Không Được Chấp Nhận
+        </h3>
+        <p className="mb-4 font-medium">
+          Khách hàng không được phép sử dụng dịch vụ của Wash&Wow để gây tổn
+          hại, xâm nhập trái phép hệ thống của chúng tôi hoặc các đối tác. Không
+          được phát tán mã độc, virus hoặc thực hiện hành vi phá hoại khác.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">4. Thanh Toán Và Hoàn Trả</h3>
+
+        <h4 className="font-semibold mb-2">4.1 Thanh Toán</h4>
+        <p className="mb-4 font-medium">
+          Khách hàng phải thanh toán đầy đủ các khoản phí theo quy định khi sử
+          dụng dịch vụ. Thông tin chi phí sẽ được hiển thị rõ ràng trước khi đặt
+          lịch giặt sấy.
+        </p>
+
+        <h4 className="font-semibold mb-2">4.2 Chính Sách Hoàn Trả</h4>
+        <p className="mb-4 font-medium">
+          Chúng tôi sẽ hoàn tiền trong các trường hợp sau: Wash&Wow ngừng cung
+          cấp dịch vụ sau khi khách hàng đã thanh toán. Dịch vụ không được cung
+          cấp trong vòng 48 giờ kể từ khi đặt hàng.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">5. Ngừng Cung Cấp Dịch Vụ</h3>
+        <p className="mb-4 font-medium">
+          Wash&Wow có quyền ngừng cung cấp dịch vụ nếu khách hàng vi phạm các
+          điều khoản sau: Cung cấp thông tin sai lệch, gây tổn hại cho hệ thống
+          hoặc đối tác.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">
+          6. Thay Đổi Điều Khoản Sử Dụng
+        </h3>
+        <p className="mb-4 font-medium">
+          Chúng tôi có thể điều chỉnh và cập nhật điều khoản sử dụng khi cần
+          thiết. Mọi thay đổi sẽ được công bố trên trang web và ứng dụng của
+          Wash&Wow, và có hiệu lực ngay sau khi công bố.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">
+          7. Chính Sách Bảo Mật Thông Tin
+        </h3>
+        <p className="mb-4 font-medium">
+          Chúng tôi cam kết bảo vệ thông tin cá nhân của khách hàng và không
+          chia sẻ cho bất kỳ bên thứ ba nào mà không có sự đồng ý của bạn, trừ
+          trường hợp pháp luật yêu cầu.
+        </p>
+
+        <h3 className="font-bold text-3xl mb-2">8. Hỗ Trợ Và Khiếu Nại</h3>
+        <p className="mb-4 font-medium">
+          Mọi thắc mắc hoặc khiếu nại liên quan đến việc sử dụng dịch vụ của
+          Wash&Wow, xin vui lòng liên hệ với chúng tôi qua
+          hello.washnwow@gmail.com. Chúng tôi cam kết phản hồi trong vòng 24 giờ
+          làm việc.
+        </p>
+      </div>
     </div>
   );
 }
