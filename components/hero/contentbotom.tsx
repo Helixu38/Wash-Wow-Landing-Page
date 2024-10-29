fdfcfd73d9a7939f951263ea74bc6e390f247c8e
helixu38 <vipase2004@gmail.com>
2024-10-29 23:29:49 +0700

    feat: responsive

--- components/hero/contentbotom.tsx
+++ components/hero/contentbotom.tsx
@@ -34,40 +34,30 @@ export default function ContentBottom() {
         </div>
 
         {/* Section 4 */}
-        <div className="flex flex-col">
-          <div className="flex flex-row small-mobile:justify-center sm:justify-center md:items-star">
-            <Image
-              src="/img/hero/4.png"
-              alt="Hero Image"
-              width={134.4}
-              height={266}
-              unoptimized={true}
-              className="object-cover w-[134.4px] h-[266px] sm:w-[200px] sm:h-[400px] md:w-[345px] md:h-[690px]"
-            />
-            <Image
-              src="/img/hero/5.png"
-              alt="Hero Image"
-              width={134.4}
-              height={266}
-              unoptimized={true}
-              className="object-cover w-[134.4px] h-[266px] sm:w-[200px] sm:h-[400px] md:w-[345px] md:h-[690px]"
-            />
-          </div>
-          <div className="flex flex-col small-mobile:text-center sm:text-center">
-            <h2 className="font-bold small-mobile:text-lg sm:text-3xl mt-[14px]">
-              Theo dõi tình trạng và đơn hàng tiện lợi
-            </h2>
-            <p className="font-medium small-mobile:text-base sm:text-lg my-[25px]">
+        <div className="flex flex-row items-start max-w-4xl mx-auto space-x-4 p-4 mb-[234px]">
+          <div className="flex flex-col small-mobile:space-y-3 text-left sm:mt-[54px] md:mt-[174px]">
+            <p className="text-base font-bold sm:text-lg sm:mb-[37px] md:text-3xl">
+              Theo dõi tình trạng <br /> và đơn hàng tiện lợi
+            </p>
+            <p className="text-sm font-medium sm:text-base md:pr-[62px]">
               Wash&Wow cung cấp cho đối tác công cụ theo dõi tình trạng và quản
               lý đơn hàng một cách tiện lợi. Đối tác có thể dễ dàng cập nhật
               trạng thái giặt sấy, quản lý các đơn hàng và phục vụ khách hàng
               hiệu quả, giúp tối ưu hóa quy trình làm việc và tăng cường chất
               lượng dịch vụ.
             </p>
+            <Button className="bg-primary text-lg md:mr-[62px] md:mt-[24px] h-[45px] rounded-xl mb-[61px]">
+              Đăng ký ngay
+            </Button>
           </div>
-          <Button className="bg-primary text-lg small-mobile:mx-[60px] sm:mx-[60px] h-[45px] rounded-xl mb-[61px]">
-            Đăng ký ngay
-          </Button>
+          <Image
+            src="/img/hero/1.png"
+            alt="Hero Image"
+            width={134.4}
+            height={266}
+            unoptimized={true}
+            className="object-cover w-[134.4px] h-[266px] sm:w-[200px] sm:h-[400px] md:w-[345px] md:h-[690px] "
+          />
         </div>
       </div>
     </div>
