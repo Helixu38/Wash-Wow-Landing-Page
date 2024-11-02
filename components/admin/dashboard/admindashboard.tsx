ccf52a25120fcf15c584b4e18dca0abc68e6f466
helixu38 <vipase2004@gmail.com>
2024-11-03 05:10:09 +0700

    feat: responsive

--- components/admin/dashboard/admindashboard.tsx
+++ components/admin/dashboard/admindashboard.tsx
@@ -19,8 +19,8 @@ function AdminDashboard() {
   }, []);
 
   return (
-    <div className="flex flex-col lg:flex-row gap-[25px]">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-[25px] flex-grow-0">
+    <div className="flex flex-col lg:flex-row gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:lg:grid-cols-2 lg:grid-cols-2 gap-6 flex-grow">
         <AdminCard
           title="Customers"
           numberStat={
@@ -45,20 +45,18 @@ function AdminDashboard() {
             stats ? `${stats.value.commissionRate.toString()}%` : "Loading..."
           }
         />
-
         <AdminCard
           title="Actual Revenue"
           numberStat={stats ? stats.value.realRevenue.toString() : "Loading..."}
         />
-
         <AdminCard
           title="Total Transactions"
           numberStat={
-            stats ? stats.value.totalCustomers.toString() : "Loading..."
+            stats ? stats.value.totalTransactions.toString() : "Loading..."
           }
         />
       </div>
-      <div className="flex-grow h-full lg:w-1/2 w-full mt-4 lg:mt-0">
+      <div className="flex-grow h-full mt-4 lg:mt-0 lg:w-1/2">
         {stats && (
           <AdminChart monthlyRevenueGrowth={stats.value.monthlyRevenueGrowth} />
         )}
